890b9717b5af2e1fc13b3c416dd7ca1ba8a2b664
Giovane Oliveira <35038647+Giovane-Oliveira@users.noreply.github.com>
2019-07-11 05:01:26 -0300

    Update

---
+++ list/src/Page1.js
@@ -0,0 +1,63 @@
+// src/Page1.js
+
+import React from 'react';
+import { View, Button, StyleSheet, AppRegistry, Image } from 'react-native';
+
+
+const Page1 = ({ navigation }) => (
+  <View style = {style.container}>
+    <View style = {style.btn}>
+    <View style = {style.n}>
+      <Button 
+        title="NOVA LISTA"
+        onPress={() => navigation.navigate('Novo') }
+        color =  'black'
+      />
+      </View>
+      <View style = {style.h}>
+      <Button 
+        title="HISTÓRICO"
+        onPress={() => navigation.navigate('Historico') }
+        color =  'black'
+      />
+      </View>
+      <View style = {style.f}>
+      <Button 
+        title="GRUPOS"
+        onPress={() => navigation.navigate('Grupos') }
+        color =  'black'
+      />
+      </View>
+    </View>
+  </View>
+);
+
+Page1.navigationOptions = {
+  title: 'LIST.IT',
+}
+
+const style = StyleSheet.create ({
+  container : {
+    flex: 1,
+    backgroundColor: 'lightgray',
+    justifyContent: 'center'
+
+  },
+  btn: {
+
+    margin: 50,
+  },
+  n: {
+    backgroundColor: 'red',
+    marginBottom: 10,
+    textShadowColor: 'black'
+  },
+  h: {
+    marginBottom: 10,
+  },
+  f: {
+    marginBottom: 10,
+  },
+});
+
+export default Page1;
---
+++ list/src/index.js
@@ -0,0 +1,19 @@
+// src/index.js
+
+import Page1 from './Page1';
+import Page2 from './Page2';
+import Page3 from './Page3';
+import Page4 from './Page4';
+
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+
+const Routes = createAppContainer(
+  createStackNavigator({
+    Home: Page1,
+    Novo: Page2,
+    Historico: Page3,
+    Grupos: Page4,
+  })
+);
+
+export default Routes;
